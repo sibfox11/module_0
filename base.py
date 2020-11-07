a36b60a9723e37a142558feec23290bf68f531fd
sibfox11 <33192212+sibfox11@users.noreply.github.com>
2020-11-07 15:24:05 +0700

    Первая рабочая версия

--- base.py
+++ base.py
@@ -1,7 +1,9 @@
+import numpy as np
+
 def game_core_v2(number):
     '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
        Функция принимает загаданное число и возвращает число попыток'''
-     count = 1
+    count = 1
     predict = np.random.randint(1,101)
     while number != predict:
         count+=1
@@ -9,6 +11,33 @@ def game_core_v2(number):
             predict += 1
         elif number < predict: 
             predict -= 1
+    return(count) # выход из цикла, если угадали
+
+def game_core_v3(number):
+    '''В отличие от v2 не просто предлагаем случайное число в качестве ответа, а берем середину диапазона, в котором загадано число, 
+	тем самым вдвое уменьшая этот диапазон при каждой попытке.
+    Функция принимает загаданное число и возвращает число попыток'''
+    
+    count = 1
+    l_border = 0 # Нижняя граница диапазона,не входит в диапазон, упрощаем алгоритм.
+    u_border = 101 #Верхняя граница диапазона, по традиции, не входит в диапазон 
+    
+    
+    predict = (u_border - l_border)//2
+    while number != predict:
+        print('Predict={} l_border={} u_border={}'.format(predict,l_border,u_border))
+        count += 1
+        
+        if number > predict:
+            l_border = predict
+            predict += (u_border - l_border)//2
+            #predict += 1
+        elif number < predict: 
+            u_border=predict
+            predict -= (u_border - l_border)//2
+        if count > 101:
+            break
+    print('Загаданное число=',predict)
     return(count) # выход из цикла, если угадали
 
 
